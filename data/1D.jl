05d1dbf57f75afe240ebeb48792de1f377e680db
Rahul Manavalan <rahul.learns.physics@gmail.com>
2022-07-25 16:32:03 +0200

    Add scripts to generate data

---
+++ data/1D.jl
@@ -0,0 +1,19 @@
+## 1D Automatic
+begin
+    dims = OneD()
+    ninstances = 10
+    nx = 1000
+    nt = 100
+    xmin = 0.0
+    xmax = 1.0
+    tmin = 0.0
+    tmax = 1.0 
+    x = xmin : (xmax-xmin)/nx : xmax
+    t = tmin : (tmax-tmin)/nt : tmax
+    method = FiniteDiff()
+    s = x 
+
+    prob = EnsembleProblem(dims,method,s,t,ninstances)
+    sol = solve(prob,acousticWE)
+    write(sol,dims)
+end 
---
+++ data/2D.jl
@@ -0,0 +1,22 @@
+begin
+    dims = TwoD()
+    ninstances = 10
+    nx = 100
+    ny = 100
+    nt = 100
+    xmin = 0.0
+    xmax = 1.0
+    ymin = 0.0
+    ymax = 1.0
+    tmin = 0.0
+    tmax = 5.0
+    x = xmin : (xmax-xmin)/nx : xmax
+    y = ymin : (ymax-ymin)/ny : ymax
+    t = tmin : (tmax-tmin)/nt : tmax
+    s = (x,y)
+    method = FiniteDiff()
+
+    prob = EnsembleProblem(dims,method,s,t,ninstances)
+    sol = solve(prob,acousticWE)
+    write(sol,dims)
+end 
