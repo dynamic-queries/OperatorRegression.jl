c3d409e4f9a8aba13e4a1115351512f8ad0dbe31
Rahul Manavalan <rahul.learns.physics@gmail.com>
2022-08-02 16:17:14 +0200

    FNO Auto-DOE script

--- test/FourierNet/FNO.jl
+++ test/FourierNet/FNO.jl
@@ -35,35 +35,37 @@ train_loader = Flux.DataLoader(train_data,batchsize=10,shuffle=true)
 test_loader = Flux.DataLoader(test_data,batchsize=10,shuffle=false)
 
 
-DL = 32
-nmodes = 16
-model = Chain(
-        Dense(2,DL),
-        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
-        Dense(DL,DL),
-        Dense(DL,1)
-)
+dls = [32,64,128,256,512,1024]
+for DL in dls 
+        nmodes = 16
+        model = Chain(
+                Dense(2,DL),
+                OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+                OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+                OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+                OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+                Dense(DL,DL),
+                Dense(DL,1)
+        )
 
-opt = Flux.Optimiser(WeightDecay(1e-4),Flux.ADAM(1e-3))
-lossfunction = l₂loss
-data = (train_loader,test_loader)
+        opt = Flux.Optimiser(WeightDecay(1e-4),Flux.ADAM(1e-3))
+        lossfunction = l₂loss
+        data = (train_loader,test_loader)
 
-learner = Learner(model,data, opt,lossfunction)
-fit!(learner,100)
+        learner = Learner(model,data, opt,lossfunction,Checkpointer(joinpath(@__DIR__,"checkpoints/$(DL)/")))
+        fit!(learner,1e3)
 
-# Validate a model 
-iter = [1,10,25,30,50,60,75,80,100]
+        # Validate model 
+        iter = [1,10,25,30,50,60,75,80,100]
 
-for i in iter
-        x = input[2,:,i]
-        a = input[1,:,i]
-        u = output[1,:,i]
-        val_i = input[:,:,i]
-        trial = model(reshape(val_i,(2,1001,1)))[1,:,1]
-        plot(x,trial,label="trial",xlabel="Grid - x",ylabel="Amplitude of Oscillations")
-        display(plot!(x,u,label="actual"))
-        savefig("")
+        for i in iter
+                xg = input[2,:,i]
+                ag = input[1,:,i]
+                ug = output[1,:,i]
+                val_i = input[:,:,i]
+                trial = model(reshape(val_i,(2,1001,1)))[1,:,1]
+                plot(xg,trial,label="trial",xlabel="Grid - x",ylabel="Amplitude of Oscillations")
+                display(plot!(xg,ug,label="actual",title="$(DL) - Depth of Layers"))
+                savefig(joinpath(@__DIR__,"figures/$(DL)_$(i).png"))
+        end 
 end 
