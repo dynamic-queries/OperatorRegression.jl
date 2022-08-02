b43bd58f13ae6544f1a31ffb7d3dae0ec6f34ab7
Rahul Manavalan <rahul.learns.physics@gmail.com>
2022-08-02 16:00:07 +0200

    Working FNO

--- test/FourierNet/FNO.jl
+++ test/FourierNet/FNO.jl
@@ -17,9 +17,9 @@ plot!(u[:,end])
 
 input = zeros(2,size(u,1),100)
 output = zeros(1,size(u,1),100)
-input[1,:,:] = u[:,100]
-input[2,:,:] = x[:,100]
-output[1,:,:] = u[:,100]
+input[1,:,:] = a[:,:]
+input[2,:,:] = x[:,:]
+output[1,:,:] = u[:,:]
 
 # Make sure munging is correct
 idx = 13
@@ -27,34 +27,43 @@ plot(input[1,:,idx])
 plot!(output[1,:,idx])
 
 # Setup the FNO net
-num_test = 5
-ntrain = 50-num_test
-
-x = input[:,:,1:end-1]
-y = output[:,:,2:end]
+x = input
+y = output
 
 train_data,test_data  = Flux.splitobs((x,y),at=0.90)
+train_loader = Flux.DataLoader(train_data,batchsize=10,shuffle=true)
+test_loader = Flux.DataLoader(test_data,batchsize=10,shuffle=false)
 
-train_loader = Flux.DataLoader(train_data, batchsize=5, shuffle=true)
-test_loader = Flux.DataLoader(test_data, batchsize=1, shuffle=false)
 
-DL = 256
+DL = 32
+nmodes = 16
 model = Chain(
-        Dense(1,DL),
-        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
+        Dense(2,DL),
+        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (nmodes,), FourierTransform, gelu),
         Dense(DL,DL),
         Dense(DL,1)
 )
 
-train_data[1][:,:,1]
-
-
-loss(x,y) = Flux.Losses.mse(model(x),y)
-optimizer = Flux.Optimiser(WeightDecay(1e-4),Flux.ADAM(1e-3))
+opt = Flux.Optimiser(WeightDecay(1e-4),Flux.ADAM(1e-3))
+lossfunction = l₂loss
 data = (train_loader,test_loader)
 
-learner = Learner(model,data,optimizer,loss,Checkpointer(joinpath(@__DIR__,"checkpoints/")))
-fit!(learner,200)
+learner = Learner(model,data, opt,lossfunction)
+fit!(learner,100)
+
+# Validate a model 
+iter = [1,10,25,30,50,60,75,80,100]
+
+for i in iter
+        x = input[2,:,i]
+        a = input[1,:,i]
+        u = output[1,:,i]
+        val_i = input[:,:,i]
+        trial = model(reshape(val_i,(2,1001,1)))[1,:,1]
+        plot(x,trial,label="trial",xlabel="Grid - x",ylabel="Amplitude of Oscillations")
+        display(plot!(x,u,label="actual"))
+        savefig("")
+end 
