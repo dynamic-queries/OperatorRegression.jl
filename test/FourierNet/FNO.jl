3c2ddf65ec25461b5796c5ba530192942c3d7317
Rahul Manavalan <ge96gak2@cm2login3.cos.lrz.de>
2022-08-02 14:43:55 +0200

    Change and test

--- test/FourierNet/FNO.jl
+++ test/FourierNet/FNO.jl
@@ -6,7 +6,7 @@ using FluxTraining
 using Flux:Dense
 
 # Weed out data
-filename = "WaveSurrogates.jl/data/End_1D_Wave_Equation"
+filename = "data/End_1D_Wave_Equation"
 file = h5open(filename)
 a,x,t,u = read(file["a"]),read(file["x"]),read(file["t"]),read(file["u"])
 u = u[:,end,:]
@@ -15,16 +15,11 @@ u = u[:,end,:]
 plot(a[:,end])
 plot!(u[:,end])
 
-
-# Write munge function 
-required_time_step = 99
-timesteps_to_train = collect(100-50:99)
-
-input = zeros(1,size(u,1),length(timesteps_to_train))
-output = zeros(1,size(u,1),length(timesteps_to_train))
-input[1,:,:] = u[:,timesteps_to_train]
-# input[2,:,:] = x[:,timesteps_to_train]
-output[1,:,:] = u[:,Int.(timesteps_to_train.+1)]
+input = zeros(2,size(u,1),100)
+output = zeros(1,size(u,1),100)
+input[1,:,:] = u[:,100]
+input[2,:,:] = x[:,100]
+output[1,:,:] = u[:,100]
 
 # Make sure munging is correct
 idx = 13
@@ -38,28 +33,28 @@ ntrain = 50-num_test
 x = input[:,:,1:end-1]
 y = output[:,:,2:end]
 
-train_data,test_data  = Flux.splitobs((x,y),at=0.95)
+train_data,test_data  = Flux.splitobs((x,y),at=0.90)
 
-train_loader = Flux.DataLoader(train_data, batchsize=47, shuffle=true)
-test_loader = Flux.DataLoader(test_data, batchsize=2, shuffle=true)
+train_loader = Flux.DataLoader(train_data, batchsize=5, shuffle=true)
+test_loader = Flux.DataLoader(test_data, batchsize=1, shuffle=false)
 
-DL = 64
+DL = 256
 model = Chain(
         Dense(1,DL),
-        OperatorKernel(DL=>DL, (16,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (16,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (16,), FourierTransform, gelu),
-        OperatorKernel(DL=>DL, (16,), FourierTransform, gelu),
-        Dense(DL,128),
-        Dense(128,1)
+        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
+        OperatorKernel(DL=>DL, (20,), FourierTransform, gelu),
+        Dense(DL,DL),
+        Dense(DL,1)
 )
 
 train_data[1][:,:,1]
 
 
 loss(x,y) = Flux.Losses.mse(model(x),y)
-optimizer = Flux.Optimiser(WeightDecay(1e-4),Flux.Adam(1e-5))
+optimizer = Flux.Optimiser(WeightDecay(1e-4),Flux.ADAM(1e-3))
 data = (train_loader,test_loader)
 
 learner = Learner(model,data,optimizer,loss,Checkpointer(joinpath(@__DIR__,"checkpoints/")))
-fit!(learner,100)
+fit!(learner,200)
